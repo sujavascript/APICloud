fb32febadba4515cfd7345891e310fdd798510b7
sujavascript <2235427085@qq.com>
2020-04-20 15:37:08 +0800

    增加底部导航

--- widget/script/common.js
+++ widget/script/common.js
@@ -1,13 +1,20 @@
 //常用自定义变量
 var AppDefine={
   debug:true,//调式开关,开发环境true,线上环境false,
-  httpServe:'',//API地址域名
+  tabFooterUrlList:['home','find','showroom','broker','my'],//底部tab链接
+  httpServe:'http://appapi.iyimaiche.com',//API地址域名
 }
 //常用公共方法
 var AppFun={
-
+  tabFooterNavChange:function(index){
+    api.openFrame({
+        name:AppDefine.tabFooterUrlList[index],
+        url: AppDefine.tabFooterUrlList[index]+'.html',
+        bounces:false,
+    });
+  }
 }
 //api调用接口url统一管理
 var ApiUrl={
-
+  GetClueOrder:AppDefine.httpServe+'/api/General/GetClueOrder',//线索排名
 }
